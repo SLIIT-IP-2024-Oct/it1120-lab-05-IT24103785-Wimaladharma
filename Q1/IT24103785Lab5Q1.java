d005a847bd326c33208afe3ca2d4ba9e37f4a265
IT24103785-Wimaladharma <IT24103785@my.sliit.lk>
2025-01-09 15:46:52 +0530

    Add files via upload

---
+++ Q1/IT24103785Lab5Q1.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class IT24103785Lab5Q1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the first integer: ");
+    int num1 = input.nextInt();
+    System.out.print("Enter the second integer: ");
+    int num2 = input.nextInt();
+    System.out.println("Enter the third integer:");
+    int num3 = input.nextInt();
+
+    System.out.println("User input numbers are: " + num1 + " " + num2 + " " + num3);
+  }
+}
